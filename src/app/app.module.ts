72080c6448678a25d0046a5860eda6d20527ae07
tipparaju2010 <tipparaju2010@gmail.com>
2021-10-10 16:51:06 -0500

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { TrackComponent } from './home/track/track.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, HeaderComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HeaderComponent,
+    HomeComponent,
+    TrackComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
